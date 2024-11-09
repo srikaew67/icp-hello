7f378789f82840f3eb4852ce1011cea12dcd0409
TNK <srikaew.2003@gmail.com>
2024-11-09 19:24:30 +0700

    add show/hide name button

--- src/hello_world_frontend/src/App.vue
+++ src/hello_world_frontend/src/App.vue
@@ -1,15 +1,29 @@
 <script setup>
 import { ref } from 'vue';
 import { hello_world_backend } from 'declarations/hello_world_backend/index';
+
 let greeting = ref('');
+let names = ref([]);
+let showNames = ref(false);
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
   const name = target.querySelector('#name').value;
+
   await hello_world_backend.greet(name).then((response) => {
     greeting.value = response;
   });
+}
+
+async function toggleNames() {
+  // Toggle the visibility of the names list
+  showNames.value = !showNames.value;
+
+  if (showNames.value) {
+    // Fetch the submitted names from the backend if list is being shown
+    names.value = await hello_world_backend.getSubmittedNames();
+  }
 }
 </script>
 
@@ -24,5 +38,13 @@ async function handleSubmit(e) {
       <button type="submit">Click Me!</button>
     </form>
     <section id="greeting">{{ greeting }}</section>
+
+    <button @click="toggleNames">
+      {{ showNames ? 'Hide' : 'Show' }} All Names
+    </button>
+
+    <ul v-if="showNames">
+      <li v-for="(name, index) in names" :key="index">{{ name }}</li>
+    </ul>
   </main>
 </template>
